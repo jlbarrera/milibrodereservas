31e2105a117b06ffe52cd15ab658ffc2ff6fbbe8
José Luis Barrera <jlbarrera@ayesa.com>
2019-03-24 14:39:23 +0100

    Frament list now read data from SQLite

--- app/src/main/java/com/milibrodereservas/BookingFragmentList.java
+++ app/src/main/java/com/milibrodereservas/BookingFragmentList.java
@@ -1,11 +1,16 @@
 package com.milibrodereservas;
 
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +18,7 @@ import android.view.ViewGroup;
  */
 public class BookingFragmentList extends Fragment {
 
+    BookingSQLiteOpenHelper db;
 
     public BookingFragmentList() {
         // Required empty public constructor
@@ -22,8 +28,26 @@ public class BookingFragmentList extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final ArrayList<String> list = new ArrayList<String>();
+
+        db = new BookingSQLiteOpenHelper(getActivity());
+        Cursor bookings = db.getBookings();
+        if (bookings.moveToFirst()) {
+            do {
+                list.add(bookings.getString(1));
+            } while (bookings.moveToNext());
+        }
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_booking_fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_booking_fragment_list, container, false);
+
+        final ListView listview = view.findViewById(R.id.booking_list);
+        final ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/milibrodereservas/BookingSQLiteOpenHelper.java
@@ -0,0 +1,62 @@
+package com.milibrodereservas;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BookingSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "MyBookings.db";
+
+    public BookingSQLiteOpenHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE bookings(id int PRIMARY KEY, name text)");
+        populate(db);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS bookings");
+        db.execSQL("CREATE TABLE bookings(id int PRIMARY KEY, name text)");
+        populate(db);
+    }
+
+    public Cursor getBookings(){
+        /**
+         * Returns all Bookings
+         */
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columnas = new String[] {"id", "name"};
+        Cursor cursor = db.query(true, "bookings", columnas, null, null, null, null, null, null);
+        return cursor;
+    }
+
+    private void populate(SQLiteDatabase db) {
+        String[] values = new String[] { "ANTONIO", "CARMEN", "FRANCISCO",
+                "ANDREA", "JAVIER", "NURIA", "MERCEDES", "CRISTINA",
+                "Linux", "SONIA", "TERESA", "JOSE MANUEL", "ENRIQUE", "DIEGO",
+                "MONICA", "CLAUDIA", "JOAQUIN", "ANA MARIA", "PILAR", "ANA",
+                "SANDRA", "VERONICA", "VICENTE", "ANTONIO", "CARMEN", "FRANCISCO",
+                "ANDREA", "JAVIER", "NURIA", "MERCEDES", "CRISTINA",
+                "Linux", "SONIA", "TERESA", "JOSE MANUEL", "ENRIQUE", "DIEGO",
+                "MONICA", "CLAUDIA", "JOAQUIN", "ANA MARIA", "PILAR", "ANA",
+                "SANDRA", "VERONICA", "VICENTE", "ANTONIO", "CARMEN", "FRANCISCO",
+                "ANDREA", "JAVIER", "NURIA", "MERCEDES", "CRISTINA",
+                "Linux", "SONIA", "TERESA", "JOSE MANUEL", "ENRIQUE", "DIEGO",
+                "MONICA", "CLAUDIA", "JOAQUIN", "ANA MARIA", "PILAR", "ANA",
+                "SANDRA", "VERONICA", "VICENTE" };
+
+        for (int i=0; i<values.length; i++) {
+            ContentValues new_booking = new ContentValues();
+            new_booking.put("id", i+1);
+            new_booking.put("name", values[i]);
+            db.insert("bookings", null, new_booking);
+        }
+    }
+}
